512f5ede1a471e9f3056d808b071711559a267fd
maxludden <dev@maxludden.com>
2023-07-25 02:49:24 -0400

    renamed maxlogo

--- examples/gradient.py
+++ examples/gradient.py
@@ -1,11 +1,17 @@
 # pylint: disable=E0402, E0401
-import maxgradient as mg
+from lorem_text import lorem
+from maxgradient.console import Console
+from maxgradient.gradient import Gradient
+from rich.panel import Panel
 from maxgradient.theme import GradientTerminalTheme
 
+TEXT = lorem.paragraphs(2)
 
-console = mg.Console(width=40, record=True)
-
+console = Console(width=40,record=True)
+console.line(2)
 console.gradient("    Hello, World!")
+console.line(2)
+
 console.save_svg(
     "Images/hello_world.svg",
     title="Hello, World! Result",
